53de57f832725bab74de99b4ae94feb5136c5c25
sslivkoff <stormslivkoff@gmail.com>
2024-01-23 23:19:13 +0000

    fix end index in fill_missing_rows()

--- state_growth/df_utils.py
+++ state_growth/df_utils.py
@@ -7,7 +7,8 @@ import polars as pl
 
 def fill_missing_rows(
     df: pl.DataFrame,
-    index_col: str,
+    *,
+    index_col: str = 'block_number',
     min_value: int | None = None,
     max_value: int | None = None,
 ) -> pl.DataFrame:
@@ -15,5 +16,6 @@ def fill_missing_rows(
         min_value = typing.cast(int, df[index_col].min())
     if max_value is None:
         max_value = typing.cast(int, df[index_col].max())
-    full = pl.DataFrame(pl.Series(index_col, range(min_value, max_value), pl.UInt32))
+    series = pl.Series(index_col, range(min_value, max_value + 1), pl.UInt32)
+    full = pl.DataFrame(series)
     return df.join(full, on=index_col, how='outer_coalesce').fill_null(0)
