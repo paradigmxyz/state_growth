36c26e73f759201042dd729c00f1cfa742c45fc4
sslivkoff <stormslivkoff@gmail.com>
2024-02-06 19:11:48 +0000

    clean raw data loading

--- state_growth/filesystem/raw_data.py
+++ state_growth/filesystem/raw_data.py
@@ -3,17 +3,19 @@ from __future__ import annotations
 import os
 import typing
 
-from typing_extensions import Unpack
+from typing_extensions import Unpack, NotRequired
 
 import polars as pl
 
 from state_growth import aggregate_dataset
 
 
-class RawGlobKwargs(typing.TypedDict, total=False):
-    min_block: int | None
-    max_block: int | None
-    network: str
+class RawGlobKwargs(typing.TypedDict):
+    data_root: str
+    datatype: str
+    min_block: NotRequired[int | None]
+    max_block: NotRequired[int | None]
+    network: NotRequired[str]
 
 
 def get_raw_glob(
@@ -30,41 +32,39 @@ def get_raw_glob(
     else:
         wildcard = '__*.parquet'
 
-    return os.path.join(data_root, datatype, network + '__' + datatype + wildcard)
+    return os.path.join(
+        data_root, network, datatype, network + '__' + datatype + wildcard
+    )
 
 
 def scan_dataset(
-    datatype: str,
-    data_root: str,
-    min_block: int | None = None,
-    max_block: int | None = None,
-    network: str = 'ethereum',
+    *,
+    columns: typing.Sequence[str] | None = None,
+    **kwargs: Unpack[RawGlobKwargs],
 ) -> pl.LazyFrame:
-    data_glob = get_raw_glob(
-        datatype=datatype,
-        data_root=data_root,
-        min_block=min_block,
-        max_block=max_block,
-        network=network,
-    )
-    scan = pl.scan_parquet(data_glob)
-    if min_block is not None:
-        scan = scan.filter(pl.col.block_number >= min_block)
-    if max_block is not None:
-        scan = scan.filter(pl.col.block_number <= max_block)
+    scan = pl.scan_parquet(get_raw_glob(**kwargs))
+
+    if columns is not None:
+        scan = scan.select(columns)
+
+    if kwargs.get('min_block') is not None:
+        scan = scan.filter(pl.col.block_number >= kwargs['min_block'])
+    if kwargs.get('max_block') is not None:
+        scan = scan.filter(pl.col.block_number <= kwargs['max_block'])
     return scan
 
 
 def load_dataset(
-    datatype: str, data_root: str, **kwargs: Unpack[RawGlobKwargs]
+    *,
+    columns: typing.Sequence[str] | None = None,
+    **kwargs: Unpack[RawGlobKwargs],
 ) -> pl.DataFrame:
-    return scan_dataset(datatype, data_root, **kwargs).collect()
+    return scan_dataset(columns=columns, **kwargs).collect()
 
 
-def load_and_aggregate(
-    datatype: str, data_root: str, **kwargs: Unpack[RawGlobKwargs]
-) -> pl.DataFrame:
-    scan = scan_dataset(datatype, data_root, **kwargs)
+def load_and_aggregate(**kwargs: Unpack[RawGlobKwargs]) -> pl.DataFrame:
+    datatype = kwargs['datatype']
+    scan = scan_dataset(**kwargs)
     return aggregate_dataset(scan, datatype).collect()
 
 
@@ -74,3 +74,4 @@ def common_prefix(str1: str, str2: str) -> str:
         if str1[i] != str2[i]:
             return str1[:i]
     return str1[:min_length]
+
